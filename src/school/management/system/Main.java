ad05b709fd01a9d5136f7a91353a7462d249cf64
Ken <51332787+Belikeemmanuel@users.noreply.github.com>
2023-09-08 08:13:20 +0100

    Update Main.java

--- src/school/management/system/Main.java
+++ src/school/management/system/Main.java
@@ -2,8 +2,6 @@ package school.management.system;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
-
 
 public class Main {
     public static void main(String[] args) {
